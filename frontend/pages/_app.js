de92b8f045044ffc71b17d9844fc5a6e02338946
David <davidanyatonwu@gmail.com>
2023-05-17 13:03:04 +0100

    Tailwind setup. 😋

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,12 @@
+import "@/styles/globals.css";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "ethereum";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
-
-export default MyApp;
