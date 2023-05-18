e6f2ea32994e6d2abbdc8ebde3f203d7a9aa9d83
David <davidanyatonwu@gmail.com>
2023-05-18 12:19:16 +0100

    Contract skeletal setup and frontend homepage. 😇

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { ConnectWallet } from "@thirdweb-dev/react";
+import Nav from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <ConnectWallet />
-      <div className="text-3xl font-bold underline">Let the game begin</div>
+      <Nav />
     </>
   );
 }
